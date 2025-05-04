a38ffba85832c0b92e4b04026ec3fd4db8b73796
Nikhil Kumar <nikhilkumar.230816@gmail.com>
2025-05-04 15:44:59 +0530

    feat: display registered patients in a table

--- src/components/PatientTable.tsx
+++ src/components/PatientTable.tsx
@@ -7,17 +7,17 @@ export default function PatientTable({ refresh }: { refresh: boolean }) {
 
     const fetchPatients = async (forceReload = false) => {
         let activeDb = db;
-    
+
         if (forceReload) {
             activeDb = new PGlite('idb://patients-db');
-            await activeDb.ready;  
+            await activeDb.ready;
         }
-    
+
         const result = await activeDb.query('SELECT * FROM patients');
         console.log('[PatientTable] Fetched:', result.rows);
         setPatients(result.rows);
     };
-    
+
 
     useEffect(() => {
         fetchPatients();
@@ -25,19 +25,15 @@ export default function PatientTable({ refresh }: { refresh: boolean }) {
 
     useEffect(() => {
         const channel = new BroadcastChannel('patient_updates');
-
         channel.onmessage = (event) => {
             console.log('[BroadcastChannel] Received:', event.data);
-
-            console.log('[BroadcastChannel] Received:', event.data, typeof event.data);
             if (event.data == 'refresh') {
-                console.log('[BroadcastChannel] Refreshing patients...');
                 fetchPatients(true);
             }
         };
 
         return () => {
-            channel.close(); 
+            channel.close();
         };
     }, []);
 
